d372578f03291fb2b81d4716df98a2fbb406f98b
joe_mycock <joe.mycock@gmail.co.uk>
2013-04-27 17:15:10 +0100

    satrt of sam

--- states/intro.lua
+++ states/intro.lua
@@ -1,11 +1,16 @@
+require "Classes/Sam"
+
 local intro = {}
 
 function intro:load()
 	self.timer = 0
+	samTex = love.graphics.newImage("SamTest.png")
+	sam = Sam({x=400,y=100},{x=0,y=0}, samTex)
 end
 
 function intro:update(dt)
 	self.timer = self.timer + dt
+	sam:update(dt)
 end
 
 function intro:draw()
@@ -13,6 +18,7 @@ function intro:draw()
 		500 + 50*math.sin(self.timer),
 		500 + 50*math.cos(self.timer),
 		100, 100)
+		sam:draw()
 end
 
 return intro
