6119948ce4a13fc4414d8901cd999f5243c4bcdc
Bart van Strien <bart.bes+projects@gmail.com>
2013-04-27 22:12:09 +0200

    TileMap accounts for padding, can load from level files, and load lvl1.txt

--- states/game.lua
+++ states/game.lua
@@ -8,10 +8,7 @@ function game:load()
 	self.timer = 0
 	samTex = cache.image("gfx/sam.png")
 	sam = Sam({x=400,y=100},{x=0,y=0}, samTex)
-	self.map = TileMap("gfx/testsheet.png", {
-		"ab8YA",
-		"77n-+",
-	})
+	self.map = TileMap.fromFile("gfx/tileset.png", "levels/lvl1.txt")
 
 	love.graphics.setBackgroundColor(200, 100, 120)
 end
