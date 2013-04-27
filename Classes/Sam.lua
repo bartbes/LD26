91e7fb11bab818c8f082556c19cd8cfcd0f349fa
joe_mycock <joe.mycock@gmail.co.uk>
2013-04-27 17:58:54 +0100

    adding movement to sam

--- Classes/Sam.lua
+++ Classes/Sam.lua
@@ -4,7 +4,7 @@ class "Sam"
 {	
 	__init__ = function(self, position, velocity, tex)
 		self.position = position or {x=0,y=0}
-		self.velocity = velocity or {x=0,y=0}
+		self.velocity = {x=0,y=0}
 		self.scroll = scroll or{x=0,y=0}
 		self.screenPosition = {x =0, y=0}
 		self.tex = tex
@@ -18,6 +18,14 @@ class "Sam"
 	update = function(self,dt)
 		self.position.x = self.position.x + self.velocity.x * dt
 		self.position.y = self.position.y + self.velocity.y * dt
+		
+		if love.keyboard.isDown("left") then
+			self.position.x = self.position.x - (5 * dt)
+		end
+		
+		if love.keyboard.isDown("right") then
+			self.position.x = self.position.x + (5 * dt)
+		end
 		
 		self.screenPosition.x = self.position.y - self.scroll.x
 		self.screenPosition.y = self.position.y - self.scroll.y
