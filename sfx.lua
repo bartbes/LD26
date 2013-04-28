a47e1b2e9476894cd6e37ffd471d5c78b5ca4d90
Bart van Strien <bart.bes+projects@gmail.com>
2013-04-28 19:40:32 +0200

    Sfx tweaking

--- sfx.lua
+++ sfx.lua
@@ -2,9 +2,18 @@ local cache = require "lib.cache"
 
 local sfx = {}
 
-function sfx.play(name)
+function sfx.play(name, looping, volume)
 	local sd = cache.soundData("sfx/" .. name .. ".ogg")
 	local sfx = love.audio.newSource(sd)
+
+	if looping then
+		sfx:setLooping(true)
+	end
+
+	if volume then
+		sfx:setVolume(volume)
+	end
+
 	sfx:play()
 	return sfx
 end
