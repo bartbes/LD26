4866368351873be1bac862f8ccca77b931d14ec3
Bart van Strien <bart.bes+projects@gmail.com>
2013-04-30 02:06:22 +0200

    Add first hatch-opening computer, clean up some old code, new fires

--- levels/lvl2.lua
+++ levels/lvl2.lua
@@ -41,11 +41,8 @@ lvl = class.private "Level2" (Level) {
 
 	getFirePositions = function(self)
 		return {
-			{65, 57},
-			{26, 37},
-			{70, 37},
-			{118, 48},
-			{94, 57},
+			{45, 60},
+			{60, 64},
 		}
 	end,
 	
@@ -70,50 +67,25 @@ lvl = class.private "Level2" (Level) {
 	end,
 
 	isDestructableTile = function(self, tile)
-		return tile == "d" or tile == "u"
+		return tile == "d"
 	end,
 
 	destroyTile = function(self, tile, x, y, map)
-		if tile == "u" then
-			self:destroyVent(map, x, y)
-			return "E"
-		end
 		if tile == "d" then
 			map:createBattery(x, y)
 			return "-"
 		end
 	end,
 
-	destroyVent = function(self, map, x, y)
-		if map.tiles[y][x] ~= 46 then return end
-		map:modifyTile(x, y, "E", true)
-		self:destroyVent(map, x-1, y)
-		self:destroyVent(map, x+1, y)
-		self:destroyVent(map, x, y-1)
-		self:destroyVent(map, x, y+1)
-	end,
-
 	activateTerminal = function(self, map, x, y)
 		local minigame = Hacking()
 
-		if y == 29 and x >= 76 and x <= 77 then
+		if y == 66 and x >= 75 and x <= 76 then
 			function minigame.callback(success)
 				if success then
-					map:modifyTile(58, 38, "t")
+					map:modifyTile(88, 62, "t")
 					map.state.score = map.state.score + 10
 					map.minigame = Dialog("Security hatch: UNLOCKED")
-				end
-			end
-		elseif y == 48 and x >= 103 and x <= 104 then
-			function minigame.callback(success)
-				if success then
-					map:modifyTile(98, 49, "t")
-					map:modifyTile(127, 33, "+")
-					map:modifyTile(127, 32, "2")
-					map:modifyTile(127, 31, "2")
-					map.state.score = map.state.score + 10
-					self.teleporterActive = true
-					map.minigame = Dialog("Security hatch: UNLOCKED\nTeleporter: ACTIVATED")
 				end
 			end
 		end
